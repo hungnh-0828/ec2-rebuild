0f1203f9b2d726a888394e153a3c5ae591682630
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2021-12-04 16:51:26 +0700

    update path to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, :production
-set :deploy_to, "path_to_your_app"
+set :deploy_to, "/home/nguyen.huy.hungc/deploy/apps/bunny"
 set :branch, :config_deploy
 server "54.254.199.249", user: "nguyen.huy.hungc", roles: %w(web app db)
